2d798d4c40ce5bbda51e52e5699aecc849abaf0f
Clase <Clase@1A-PC14>
2018-03-24 10:44:59 -0500

    version 1.0.1

---
+++ src/main/java/com/journey/model/vehiculo.java
@@ -0,0 +1,36 @@
+package com.journey.model;
+
+public class vehiculo {
+
+	private String nombre;
+	private Double recorrido;
+	private String vehiculo;
+	private Double resultado;
+	
+	public Double getResultado() {
+		return resultado;
+	}
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getRecorrido() {
+		return recorrido;
+	}
+	public void setRecorrido(Double recorrido) {
+		this.recorrido = recorrido;
+	}
+	public String getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(String vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	
+	
+}
---
+++ src/main/java/com/journey/web/vehiculoController.java
@@ -0,0 +1,39 @@
+package com.journey.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.journey.model.vehiculo;
+
+@Controller
+public class vehiculoController {
+
+	@GetMapping("/new")
+
+	public String initCreationForm(Model model) {
+		model.addAttribute("vehiculo", new vehiculo());
+		return "journeyForm";		
+	} 
+	
+	@PostMapping("/new")	
+	public String submitForm(@ModelAttribute vehiculo vehiculo) {
+		
+		switch(vehiculo.getVehiculo()){
+			
+			case "marca1": vehiculo.setResultado(vehiculo.getRecorrido()/40);
+			break;
+			
+			case "marca2": vehiculo.setResultado(vehiculo.getRecorrido()/35);
+			break;
+			
+			case "marca3": vehiculo.setResultado(vehiculo.getRecorrido()/45);
+			break;
+		}
+		
+	return "resultFrom";
+	}
+
+}
